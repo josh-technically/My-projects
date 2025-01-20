b6527f5255aa76d0b31d97038cebac78c0eb41f9
Josh <143972736+josh-technically@users.noreply.github.com>
2025-01-20 13:17:27 -0500

    Update text.js

--- Sarcastic Text Generator/text.js
+++ Sarcastic Text Generator/text.js
@@ -1,1 +1,12 @@
+const textarea = document.querySelector('[name="text"]');
+const result = document.querySelector('.result');
+const filterInputs = document.querySelectorAll('[name="filter"]');
+
+function transformTest(text) {
+    console.log(text);
+    result.textContent = text;
+}
+
+textarea.addEventListener('input', (e) => transformTest(e.target.value));
+
 
