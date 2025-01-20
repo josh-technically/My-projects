e584a7b313ddef7da130a4dae60de6990385c990
Josh <143972736+josh-technically@users.noreply.github.com>
2025-01-20 15:46:33 -0500

    Update text.js
    adding to the existing starter code

--- Sarcastic Text Generator/text.js
+++ Sarcastic Text Generator/text.js
@@ -3,10 +3,19 @@ const result = document.querySelector('.result');
 const filterInputs = document.querySelectorAll('[name="filter"]');
 
 function transformTest(text) {
-    console.log(text);
+    // take text, turn it into an array, loop, then filter.
+    const mod = Array.from(text).map(filters.sarcastic);
+    console.log(mod);
     result.textContent = text;
 }
 
+const filters = {
+    sarcastic(letter, index) {
+        console.log(letter, index); // check and delete
+        return letter;
+    },
+    funky() {},
+    unable() {},
+};
+
 textarea.addEventListener('input', (e) => transformTest(e.target.value));
-
-
